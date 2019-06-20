5804c0a07ff20ee350c52365dc855dace33f94c4
Rahmah-Nampala <rahma10na@gmail.com>
2019-06-20 13:57:43 +0300

    contact constructor with address as an object represented by an array

--- objects.js
+++ objects.js
@@ -1,22 +1,18 @@
-// Creating a Contacts addressbook object
-function Contact(first_name, second_name, phone_number, email, address) { // creating the constructor Contact
+// Creating a Contacts addressbook object 
+
+function Contact(first_name, second_name, phone_number, email, address_array) { // creating the constructor Contact
 	this.first_name = first_name;
 	this.second_name = second_name;
 	this.phone_number = phone_number;
 	this.email = email;
-	this.address = address;
-	function Address(street, city, country) { // making the address an object inside the Contact object
-		this.street = street;
-		this.city = city;
-		this.country = country;
-	}
+	this.address = {"str": address_array[0], "city": address_array[1], "country": address_array[2]} // making the address an object inside Contact
 }
-
-var address1 = new Address("Cream road, Nsambya", "Kampala", "Uganda");
-var address2 = new Address("Kibuli road, Kabalagala", "Kampala", "Uganda");
-
-var contact1 = new Contact("Nampala", "Rahmah", 702427983, "rahma10na@gmail.com", address1);
-var contact2 = new Contact("Asio", "Rebecca", 702457986, "masio@gmail.com", address2);
-
-Contact.prototype.addfullName = function() { // String.prototype.fullName = function() {return this.first_name + "," + this.second_name};
+// address is an object that displays its information as an array
+// the individual contacts display 
+Contact.prototype.addFullName = function() { // a customised prototype to return: this.first_name + "," + this.second_name};
 	return this.first_name + "," + this.second_name};
+
+var contact1 = new Contact("Nampala", "Rahmah", 702427983, "rahma10na@gmail.com", ["Cream road, Nsambya", "Kampala", "Uganda"]);
+
+
+contact1.addFullName();
